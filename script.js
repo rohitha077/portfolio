4a509e57dc00896749e941f0d4d6dc405e30a0cc
rohitha077 <rayadurgamrohitha@gmail.com>
2026-01-19 21:59:31 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("contactForm").addEventListener("submit", function(e) {
-    e.preventDefault();
-    alert("Form submitted successfully!");
-});
-if(name.value === "" || email.value === "") {
-    alert("All fields required");
-    return;
-}
